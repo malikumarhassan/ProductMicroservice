382d8f7da49660534c9853cb8472f2ede5cf430d
malikumarhassan <malikumarhassan@live.com>
2022-10-06 10:20:06 +0400

    test

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -29,5 +29,19 @@ namespace ProductMicroservice.Controllers
             })
             .ToArray();
         }
+
+
+        //[HttpGet(Name = "GetWeatherForecastCurrent")]
+        //public IEnumerable<WeatherForecast> GetWeatherForecastCurrent()
+        //{
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+
     }
 }
